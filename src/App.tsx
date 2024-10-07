d1f53d3f3827d6a5f5619da9994900f105b4232b
Reaien <ivanduarteherrera@gmail.com>
2024-10-06 21:38:44 -0300

    initial commit with components created navbar and hero

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import HeroImage from "./components/HeroImage";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HeroImage />
+    </>
+  );
+}
+
+export default App;
