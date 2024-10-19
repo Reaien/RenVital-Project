532ac11b4863ef6cfe70bb92c30fd083483d75d7
Reaien <ivanduarteherrera@gmail.com>
2024-10-19 18:58:24 -0300

    handlerGoogleLogin and POST userDataPayload info

--- src/components/Login/CardLogin.tsx
+++ src/components/Login/CardLogin.tsx
@@ -1,10 +1,38 @@
 import { jwtDecode } from "jwt-decode";
 import logo from "../../assets/images/logo.png";
-import { GoogleLogin } from "@react-oauth/google";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 type Props = {};
 
 function CardLogin({}: Props) {
+  const handlerGoogleLogin = async (credentialResponse: CredentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    console.log(credentialResponseDecoded);
+
+    // Crear el payload con los datos del usuario
+    const userDataPayload = {
+      email: credentialResponseDecoded.email,
+      nombre: credentialResponseDecoded.name, // Asegúrate de que este sea el nombre correcto del campo
+      profile_pic: credentialResponseDecoded.picture, // Asegúrate de que este sea el nombre correcto del campo
+    };
+
+    const URL = "http://localhost:8080/users";
+
+    try {
+      await axios
+        .post(URL, userDataPayload, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then(() => {
+          console.log("enviado");
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="grid justify-center mt-32">
       <div className="mx-5 md:w-[400px] bg-white border-2 shadow-lg min-h-[280px] rounded-md">
@@ -17,12 +45,7 @@ function CardLogin({}: Props) {
         <div className="flex justify-center mt-8">
           <GoogleLogin
             width={"300"}
-            onSuccess={(credentialResponse) => {
-              const credentialResponseDecoded = jwtDecode(
-                credentialResponse.credential
-              );
-              console.log(credentialResponseDecoded);
-            }}
+            onSuccess={handlerGoogleLogin}
             onError={() => {
               console.log("Login Failed");
             }}
