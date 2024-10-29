1bcf5c4f78ddf5a85b20cb99eb57678ebbf8c51c
Reaien <ivanduarteherrera@gmail.com>
2024-10-28 22:28:03 -0300

    readded oauth2

--- src/components/Login/CardLogin.tsx
+++ src/components/Login/CardLogin.tsx
@@ -1,13 +1,43 @@
+import { jwtDecode } from "jwt-decode";
 import logo from "../../assets/images/logo.png";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 type Props = {};
 
 function CardLogin({}: Props) {
-  const handlerGoogleLogin = () => {
-    console.log("hola");
+  const handlerGoogleLogin = async (credentialResponse: CredentialResponse) => {
+    const credentialResponseDecoded = jwtDecode(credentialResponse.credential);
+    const userToken = credentialResponse.credential;
+    console.log(credentialResponse);
+    console.log(credentialResponseDecoded);
+
+    // Crear el payload con los datos del usuario
+    const userDataPayload = {
+      email: credentialResponseDecoded.email,
+      nombre: credentialResponseDecoded.name,
+      profile_pic: credentialResponseDecoded.picture,
+    };
+
+    const URL = "http://localhost:8080/users";
+
+    try {
+      await axios
+        .post(URL, userDataPayload, {
+          headers: {
+            Authorization: `Bearer ${userToken}`,
+            "Content-Type": "application/json",
+          },
+        })
+        .then(() => {
+          console.log("enviado");
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
-    <section className="grid justify-center mb-52 mt-32">
+    <section className="grid justify-center mt-32">
       <div className="mx-5 md:w-[400px] bg-white border-2 shadow-lg min-h-[280px] rounded-md">
         <div className="grid justify-center mt-4">
           <img className="h-24 ml-2" src={logo} alt="logo" />
@@ -16,12 +46,13 @@ function CardLogin({}: Props) {
           </p>
         </div>
         <div className="flex justify-center mt-8">
-          <button
-            onClick={handlerGoogleLogin}
-            className="bg-slate-100 px-12 py-4 font-bold shadow-lg border"
-          >
-            Inicia sesión con google
-          </button>
+          <GoogleLogin
+            width={"300"}
+            onSuccess={handlerGoogleLogin}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
         </div>
       </div>
     </section>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="329707270349-2gkr4g7pphd3innlfen26eufdb4poh0i.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
