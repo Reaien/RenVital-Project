fe2879e9e527430ce135d4ef0bf154b9b7346cac
Reaien <ivanduarteherrera@gmail.com>
2024-11-24 22:11:59 -0300

    fixed react auth - removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="329707270349-2gkr4g7pphd3innlfen26eufdb4poh0i.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </StrictMode>
 );
